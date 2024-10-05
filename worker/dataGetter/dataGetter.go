134a7246afaf5d909e82202edd2805195555ba43
Zephrnos <fischethan2003@gmail.com>
2024-10-04 21:20:20 -0500

    Fixes and Completions
    Working HTML Retreival

--- worker/dataGetter/dataGetter.go
+++ worker/dataGetter/dataGetter.go
@@ -28,7 +28,20 @@ func urlTickerPage(ticker string) string {
 
 func getHTML(ticker string) error {
 	url := urlTickerPage(ticker)
-	response, err := http.Get(url)
+	fmt.Printf("Fetching URL: %s\n", url)
+
+	// Create a new HTTP request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return fmt.Errorf("error creating request: %w", err)
+	}
+
+	// Set the User-Agent header to mimic a browser
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36")
+
+	// Perform the request using an HTTP client
+	client := &http.Client{}
+	response, err := client.Do(req)
 	if err != nil {
 		return fmt.Errorf("error fetching URL: %w", err)
 	}
--- worker/dataGetter/dataGetter_test.go
+++ worker/dataGetter/dataGetter_test.go
@@ -1,11 +1,23 @@
 package datagetter
 
 import (
+	"os"
 	"testing"
 )
 
 func TestHTMLGet(t *testing.T) {
-	ticker := "RIVN"
-	getHTML(ticker)
+	ticker := "AAPL"
+	err := getHTML(ticker)
+	if err != nil {
+		t.Fatalf("Failed to get HTML: %v", err)
+	}
 
+	// Check if the file was created
+	filename := ticker + ".html"
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		t.Fatalf("Expected file %s to be created, but it does not exist", filename)
+	}
+
+	// Clean up the file after test
+	defer os.Remove(filename)
 }
